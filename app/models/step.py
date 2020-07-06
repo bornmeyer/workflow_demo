17b9575c728d21fb7ac03793c0f78ba219d8ed79
Björn Meyer <aah_necroth@web.de>
2020-07-07 00:21:26 +0200

    steps will ordered via order property

--- app/models/step.py
+++ app/models/step.py
@@ -4,6 +4,9 @@ from django.db import models
 from app.models.workflow import Workflow
 
 class Step(models.Model):
+    class Meta:
+        ordering = ['order',]
+
     db_table = "steps"
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField()
@@ -11,3 +14,4 @@ class Step(models.Model):
     order = models.IntegerField(default=0)
 
     workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
+    
--- app/serializers/workflow_serializer.py
+++ app/serializers/workflow_serializer.py
@@ -4,8 +4,8 @@ from app.serializers.step_serializer import StepSerializer
 from app.serializers.comment_serializer import CommentSerializer
 
 class WorkflowSerializer(serializers.ModelSerializer):
-    steps = StepSerializer(source='step_set', many=True)
-    comments = CommentSerializer(source='comment_set', many=True)
+    steps = StepSerializer(source='step_set', many=True, read_only=True)
+    comments = CommentSerializer(source='comment_set', many=True, read_only=True)
     class Meta:
         model = Workflow
         fields = ['id', 'name', 'description', 'steps', 'comments']
