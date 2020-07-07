db8544e4bdaf1c8b5f0da9e4aabf062bddb8626f
Björn Meyer <aah_necroth@web.de>
2020-07-07 08:24:28 +0200

    user endpoint is currently exempted due to the fact that we currently have no other way to get the api key without looking it up in the database

--- app/serializers/user_serializer.py
+++ app/serializers/user_serializer.py
@@ -2,10 +2,11 @@ import uuid
 from rest_framework import serializers
 from app.models.user import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'firstname', 'lastname']
+        fields = ['id', 'email', 'firstname', 'lastname', 'api_key']
         read_only_fields = ['id', 'api_key']
 
     def create(self, validated_data):
--- app/view_sets/user_view_set.py
+++ app/view_sets/user_view_set.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from app.serializers.user_serializer import UserSerializer
 from app.models.user import User
+from rest_framework.decorators import authentication_classes, api_view
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    authentication_classes = []
+    permission_classes = []
