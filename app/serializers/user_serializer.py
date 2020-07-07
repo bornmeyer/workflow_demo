51eef51feec09ad107f312fea181986f834f132a
Björn Meyer <aah_necroth@web.de>
2020-07-07 15:23:59 +0200

    unused imports removed

--- app/serializers/user_serializer.py
+++ app/serializers/user_serializer.py
@@ -1,4 +1,3 @@
-import uuid
 from rest_framework import serializers
 from app.models.user import User
 
@@ -8,9 +7,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'firstname', 'lastname', 'api_key']
         read_only_fields = ['id', 'api_key']
-
-    #def create(self, validated_data):
-    #    new_user = User(**validated_data)
-    #    new_user.api_key = str(uuid.uuid4())
-    #    new_user.save(force_insert=True)
-    #    return new_user
--- app/view_sets/comment_view_set.py
+++ app/view_sets/comment_view_set.py
@@ -1,9 +1,7 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.response import Response
-
 from app.serializers.comment_serializer import CommentSerializer
 from app.models.workflow import Workflow
-from app.models.step import Step
 from app.models.comment import Comment
 
 class CommentViewSet(viewsets.ModelViewSet):
